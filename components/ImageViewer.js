675727886f5f11f48eda3080bc439c1be8dd4bc2
Complex <rianlotter@icloud.com>
2023-05-28 13:30:48 +0200

    init

---
+++ components/ImageViewer.js
@@ -0,0 +1,21 @@
+import { StyleSheet, Image } from 'react-native';
+
+export default function ImageViewer({ placeholderImageSource, selectedImage }) {
+
+    const imageSource = selectedImage !== null
+    ? {uri: selectedImage}
+    : placeholderImageSource
+
+  return (
+    <Image source={imageSource} style={styles.image} />
+  );
+}
+
+const styles = StyleSheet.create({
+  image: {
+    width: 400,
+    height: 540,
+    borderRadius: 18,
+    marginBottom: 20,
+  },
+});
